e89a32b364d8a9daeb69a2abe3be0b2207054002
Keerthisaisathwik <96810503+Keerthisaisathwik@users.noreply.github.com>
2022-08-11 21:23:16 +0530

    Update Category.java
    A Constructur and "getName()" method are added

--- E-CommerceOOAD/src/controller/Category.java
+++ E-CommerceOOAD/src/controller/Category.java
@@ -5,4 +5,11 @@ public class Category {
 	private String name;
 	private String description;
 	
+	public Category(String name) {
+        	this.name = name;
+    	}
+
+    	public String getName() {
+        	return name;
+    	}
 }
