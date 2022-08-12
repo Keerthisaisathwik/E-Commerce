36a20e7513c1601ccfb3ddd3013612cace14964e
Keerthisaisathwik <96810503+Keerthisaisathwik@users.noreply.github.com>
2022-08-12 22:33:38 +0530

    Update Item.java
    Accessors, Item(), updateCount() methods are added

--- E-CommerceOOAD/src/controller/Item.java
+++ E-CommerceOOAD/src/controller/Item.java
@@ -2,7 +2,32 @@ package controller;
 
 public class Item {
 
-	private Product product;
-	private double price;
-	private int count;
+     private Product product;
+     private double price;
+     private int count;
+	
+     public Item(Product product, int count) {
+        this.productId = product.getId();
+        this.count = count;
+        this.price = product.getPrice() * count;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "productId=" + productId +
+                ", count=" + count +
+                ", price=" + price +
+                '}';
+    }
+
+    public void updateCount(int newCount){
+        price = (price/count) * newCount;
+        count = newCount;
+
+    }
 }
