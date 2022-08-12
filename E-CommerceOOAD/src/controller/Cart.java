c9b96b4326592c6d1325d8b3d791b873376b5d5f
Keerthisaisathwik <96810503+Keerthisaisathwik@users.noreply.github.com>
2022-08-12 22:38:00 +0530

    Update Cart.java
    CURD methods

--- E-CommerceOOAD/src/controller/Cart.java
+++ E-CommerceOOAD/src/controller/Cart.java
@@ -4,5 +4,26 @@ import java.util.ArrayList;
 
 public class Cart {
 
-	private ArrayList<Item> items;
+    private ArrayList<Item> items;
+    
+    public Cart() {
+        this.itemList = new ArrayList<>();
+    }
+
+    public void addItem(Item item){
+        itemList.add(item);
+    }
+    public void removeItem(Item item){
+        itemList.remove(item);
+    }
+    public void updateItemCount(Item item, int newCount){
+        int index = itemList.indexOf(item);
+        itemList.get(index).updateCount(newCount);
+    }
+    public ArrayList<Item> getitemList(){
+        return itemList;
+    }
+    public void checkout(){
+        itemList.clear();
+    }
 }
