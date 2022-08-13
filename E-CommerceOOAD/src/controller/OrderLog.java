f34436887a74bc40fc1210f01bce0efc0101bf21
Keerthisaisathwik <96810503+Keerthisaisathwik@users.noreply.github.com>
2022-08-13 23:02:57 +0530

    Update OrderLog.java
    controller created

--- E-CommerceOOAD/src/controller/OrderLog.java
+++ E-CommerceOOAD/src/controller/OrderLog.java
@@ -6,4 +6,8 @@ public class OrderLog {
 
 	private Date creationTimeStamp;
 	private OrderStatus status;
+	public OrderLog(Date creationTimestamp, OrderStatus status) {
+        	this.creationTimestamp = creationTimestamp;
+        	this.status = status;
+    	}
 }
