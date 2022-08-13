000d56b50d4147a46c3adf15103f2c654e25dd02
Keerthisaisathwik <96810503+Keerthisaisathwik@users.noreply.github.com>
2022-08-13 23:09:00 +0530

    Update Shipment.java
    controller and some minor changes

--- E-CommerceOOAD/src/controller/Shipment.java
+++ E-CommerceOOAD/src/controller/Shipment.java
@@ -6,6 +6,26 @@ public class Shipment {
 
 	private int shipmentNumber;
 	private Date estimatedArrival;
-	
+	private LocalDateTime date;
+	private Order orderDetails;
 	private Order order;
+	
+	public Shipment(Order orderDetails) {
+        	numberCounter += 1;
+        	this.shipmentNumber = numberCounter;
+        	this.date = LocalDateTime.now();
+        	this.estimatedArrival = this.date.plusDays(3);
+        	this.orderDetails = orderDetails;
+        	orderDetails.setOrderStatus(OrderStatus.SHIPPED);
+    	}
+
+    	@Override
+    	public String toString() {
+        	return "Shipment{" +
+                	"shipmentNumber=" + shipmentNumber +
+                	", date=" + date +
+                	", estimatedArrival=" + estimatedArrival +
+                	", orderDetails=" + orderDetails +
+                	'}';
+    	}
 }
